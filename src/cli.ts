772a5db9db35cdcc1839d0affae2d9a1b9775977
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2023-09-15 21:14:25 +0200

    feat!: make streaming (#3)
    This PR is considered a breaking change.
    
    `MSAccess` is no longer a RDF.Store, but an RDF.Stream. That's considerably less memory sensitive.

--- src/cli.ts
+++ src/cli.ts
@@ -1,62 +1,49 @@
 #!/usr/bin/env node
 import N3 from "n3";
 import fs from "node:fs";
-import stream from "node:stream";
+import { PassThrough, pipeline as streampipeline } from "node:stream";
 import { pathToFileURL } from "node:url";
+import { promisify } from "node:util";
+import { createGzip } from "node:zlib";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import { MSAccess, MSAccessConstructorOptions } from "./msaccess.js";
+import { MSAccess } from "./msaccess.js";
 
-/** Run the quad generator and supply the CLI-arguments' buffers and context etc. */
-export default async function run(
-  inputFilePath: string,
-  outFD: stream.Writable,
-  password: string,
-  quadMode: MSAccessConstructorOptions["quadMode"],
-  baseIRI: string
-) {
-  const buffer = fs.readFileSync(inputFilePath);
-  const mdb = new MSAccess(buffer, {
-    password,
-    quadMode,
-    baseIRI: baseIRI ?? pathToFileURL(inputFilePath).href + "#",
-  });
-  const store = mdb.store();
-  const streaming = store.match() as unknown as stream.Stream;
-
-  streaming.pipe(new N3.StreamWriter({ format: "nquads" })).pipe(outFD);
-}
+const pipeline = promisify(streampipeline);
 
 export async function cli() {
   const argv = await yargs(hideBin(process.argv))
-    .option("input", { alias: "i", type: "string", description: "Database file" })
+    .option("input", { alias: "i", description: "Database file" })
     .demandOption("input")
-    .option("output", { alias: "o", type: "string", description: "Output quads file" })
+    .option("output", { alias: "o", description: "Output quads file" })
     .normalize(["input", "output"])
-    .option("password", { type: "string", description: "Password for the protected database" })
+    .option("password", { description: "Password for the protected database" })
     .requiresArg("password")
-    .option("mode", { type: "string", description: "Model used to generate quads" })
+    .option("mode", { description: "Model used to generate quads" })
     .choices("mode", ["facade-x", "csv"])
-    .option("base", { type: "string", description: "Base IRI for the Facade-X generated data" })
+    .option("base", { description: "Base IRI for the Facade-X generated data" })
+    .alias("base", "base-iri")
     .help()
     .parse();
 
   const OUT = argv.output
     ? fs.createWriteStream(argv.output, { encoding: "utf-8" })
     : process.stdout;
+  const wantsGzip = argv.output?.endsWith(".gz");
 
-  await run(
-    argv.input,
-    OUT,
-    argv.password,
-    argv.mode as MSAccessConstructorOptions["quadMode"],
-    argv.base
-  );
+  const buffer = fs.readFileSync(argv.input);
+  const parser = new MSAccess(buffer, {
+    password: argv.password as string,
+    quadMode: argv.mode as unknown as any,
+    baseIRI: (argv.base as string) ?? pathToFileURL(argv.input).href + "#",
+  });
+  const writer = new N3.StreamWriter({ format: "nquads" });
+
+  try {
+    pipeline(parser, writer, wantsGzip ? createGzip() : new PassThrough(), OUT);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-try {
-  void (await cli());
-} catch (e) {
-  console.error(e.message ?? e);
-  process.exit(1);
-}
+if (import.meta.url === pathToFileURL(process.argv[1]).href) void cli();
